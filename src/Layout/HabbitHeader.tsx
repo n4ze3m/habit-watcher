083f2bd60962448b6b74135f2c5d8b5e55d3262b
n4ze3m <n4ze3m@gmail.com>
2022-11-16 17:55:12 +0530

    feat: added version to header

--- src/Layout/HabbitHeader.tsx
+++ src/Layout/HabbitHeader.tsx
@@ -1,5 +1,7 @@
-import { Header, Text, createStyles, Container } from "@mantine/core";
+import { Header, Text, createStyles, Container, Indicator } from "@mantine/core";
 import { useNavigate } from "react-router-dom";
+import { getVersion } from '@tauri-apps/api/app';
+import { useQuery } from "@tanstack/react-query";
 const useStyles = createStyles((_) => ({
 	inner: {
 		height: 65,
@@ -15,17 +17,26 @@ const useStyles = createStyles((_) => ({
 export const HabbitHeader = () => {
 	const { classes } = useStyles();
 	const navigate = useNavigate();
+
+	const { data: version } = useQuery(["getTauriVersion"], async () => {
+		const appVersion = await getVersion();
+		return appVersion;
+	})
+
+
 	return (
 		<Header height={65}>
 			<Container className={classes.inner} fluid={true}>
-				<Text
-					onClick={() => navigate("/")}
-					className={classes.curosrPointer}
-					weight="bold"
-					size="lg"
-				>
-					Habit Watcher
-				</Text>
+				<Indicator inline={true} label={`v${version}`} offset={-4} size={12} color="teal">
+					<Text
+						onClick={() => navigate("/")}
+						className={classes.curosrPointer}
+						weight="bold"
+						size="lg"
+					>
+						Habit Watcher
+					</Text>
+				</Indicator>
 			</Container>
 		</Header>
 	);
